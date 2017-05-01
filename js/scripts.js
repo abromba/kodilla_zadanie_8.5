441765217d3dbb8e2f788686c4946ecb35521fb6
Ania <Ania>
2017-05-01 16:43:46 +0200

    .js upgraded, .css added

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var a = prompt('Podaj długość podstawy trójkąta w cm');
-  var h = prompt('Podaj wysokość trójkąta w cm');
-  var triangleArea = a*h/2
+var h = prompt('Podaj wysokość trójkąta w cm');
+var triangleArea = a * h / 2;
+
 alert('Pole trójkąta o podstawie a: ' + a + 'cm i wysokości h: ' + h + 'cm wynosi: ' + triangleArea + 'cm2');
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea+ 'cm2');
